e80a3a37411a288bfa8a048f4cda8e573bfb98b2
Yakiv Shypelyk <schypelyk128@ukr.net>
2020-10-28 22:18:23 +0200

    Перща вдала спроба

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import './styles.css';
+import menu from './menu.json';
+
+import menuTemplate from '../templates/menu-cards.hbs';
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+// menu
+
+const menuItems = document.querySelector('.js-menu');
+const body = document.querySelector('body');
+
+const markup = menuTemplate(menu);
+
+menuItems.insertAdjacentHTML('beforeend', markup);
+
+// BODY THEME
+
+const themeSwitchToggle = document.querySelector('.theme-switch__toggle');
+
+themeSwitchToggle.addEventListener('change', onChangeSwitch);
+themeSwitchToggle.addEventListener('change', onLocalStorage);
+document.addEventListener('themeLoaded', onGetThemeLocalStorage); 
+
+function onChangeSwitch (evt) {
+    if (themeSwitchToggle.checked){
+        setDarkTheme()
+    } else {        
+        setLightTheme()
+    }
+}
+
+function setDarkTheme() {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+}
+
+function setLightTheme() {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+  }
+
+// Local Storage
+
+function onLocalStorage(evt) {
+    if (themeSwitchToggle.checked) {
+        localStorage.setItem('theme', Theme.DARK);
+    } else {
+        localStorage.setItem('theme', Theme.LIGHT);
+    }
+}
+ 
+function onGetThemeLocalStorage() {
+    const themeInLocalStrg = localStorage.getItem('theme');
+    if (themeInLocalStrg === Theme.DARK) {
+      body.classList.add(Theme.DARK);
+      themeSwitchToggle.checked = true;
+    }
+  } 
